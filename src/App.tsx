4a8dbf6307f4640591c271ab351761cc47c55b52
Matt <matthew@wearespectacular.com>
2024-12-04 21:32:58 +0000

    task[BrightHR] - create useAbsences hook and absenceTableHelpers, styling including dark / light func

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Header from './components/Header';
-import AbsenceTable from './components/AttendanceTable/AbsenceTable';
+import AbsenceTable from './components/AbsenceTable/AbsenceTable';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App bg-white dark:bg-gray-800'>
       <Header />
       <div className='w-full mx-auto md:w-[800px] overflow-x-scroll'>
         <AbsenceTable />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bright-main': '#0057ad'
+      },
+      backgroundImage: {
+        'bright-gradient': 'linear-gradient(355deg, #ffb8d4 -20%, #0070e0 100%, #0070e0 0)'
+      },
+    },
   },
   plugins: [],
 }
